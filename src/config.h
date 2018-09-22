cc057da792b144790df35ec1f91bd30cc9cd489f
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-09-23 04:12:02 +0530

    Create config file

---
+++ src/config.h
@@ -0,0 +1,19 @@
+//
+// Created by Sumedhe Dissanayake on 10/3/18.
+//
+#include<string>
+
+using namespace std;
+
+#ifndef SOM_CONFIG_H
+#define SOM_CONFIG_H
+
+// App config
+const string INPUT_PATH = "../resources/input.csv";
+
+// Model config
+const int DEFAULT_HEIGHT    = 10;
+const int DEFAULT_WIDTH     = 10;
+const int DEFAULT_DIMENTION = 3;
+
+#endif //SOM_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include "config.h"
+#include "utils/csv_loader.h"
+
+using namespace std;
 
 int main() {
     std::cout << "Program is running..." << std::endl;
+    if (errno) cerr << "Error: " << strerror(errno);
     return 0;
 }
