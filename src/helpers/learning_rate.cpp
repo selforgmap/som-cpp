3f1ad8813f3dcd08a619a1290a06996845bf6313
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-10-19 08:48:07 +0530

    Create learning rate functions

--- src/helpers/learning_rate.cpp
+++ src/helpers/learning_rate.cpp
@@ -4,6 +4,20 @@
 
 #include "learning_rate.h"
 
+#include <math.h>
+
 float constant_learning_rate(float starting_learning_rate){
     return starting_learning_rate;
+}
+
+float linear_learning_rate(float starting_learning_rate, int iteration){
+    return 1.0f * starting_learning_rate * (1 / 1.0f * iteration);
+}
+
+float inverse_of_time_learning_rate(float starting_learning_rate, int iteration, int iteration_limit){
+    return starting_learning_rate * (1 - iteration / 1.0f * iteration_limit);
+}
+
+float power_series_learning_rate(float starting_learning_rate, int iteration, int iteration_limit){
+    return (float)(starting_learning_rate * exp(iteration / iteration_limit));
 }
--- src/helpers/learning_rate.h
+++ src/helpers/learning_rate.h
@@ -12,4 +12,33 @@
  */
 float constant_learning_rate(float starting_learning_rate);
 
+/**
+ * Linear Learning Rate
+ * alpha(0) * (1 / t)
+ * @param starting_learning_rate Starting learning rate
+ * @param iteration Current Iteration
+ * @return Learning rate
+ */
+float linear_learning_rate(float starting_learning_rate, int iteration);
+
+/**
+ * Inverse of time function for learning rate
+ * alpha(0) * (1 - t/T)
+ * @param starting_learning_rate Starting learning rate
+ * @param iteration Current iteration
+ * @param iteration_limit Maximum iterations
+ * @return Learning rate
+ */
+float inverse_of_time_learning_rate(float starting_learning_rate, int iteration, int iteration_limit);
+
+/**
+ * Power series function for learning rate
+ * alpha(0) * e^(t/T)
+ * @param starting_learning_rate Starting learning rate
+ * @param iteration Current iteration
+ * @param iteration_limit Iteration limit
+ * @return Learning rate
+ */
+float power_series_learning_rate(float starting_learning_rate, int iteration, int iteration_limit);
+
 #endif //SOM_LEARNING_RATE_H
